08d47388244f10c1cac45e5932af8fb6b2d99671
Angular CLI <angular-cli@angular.io>
2017-07-04 21:05:47 -0700

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { QualityExercisesJavascriptPage } from './app.po';
+
+describe('quality-exercises-javascript App', () => {
+  let page: QualityExercisesJavascriptPage;
+
+  beforeEach(() => {
+    page = new QualityExercisesJavascriptPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
